9ee11e8c0566a2384dccec7a157514700827a7b8
AndrewLevada <zipeofficial@gmail.com>
2021-07-02 00:16:40 +0300

    Fixed MenuItems

--- Assets/Scripts/Editor/MenuItems.cs
+++ Assets/Scripts/Editor/MenuItems.cs
@@ -1,15 +1,7 @@
-using General;
 using UnityEditor;
-using UnityEditor.SceneManagement;
 
 namespace Editor {
 public static class MenuItems {
-	[MenuItem("Simulation/Go back to Main")]
-	private static void goBackToMain() {
-		EditorSceneManager.SaveOpenScenes();
-		EditorSceneManager.OpenScene(Paths.mainScenePath);
-	}
-	
 	[MenuItem("Simulation/Open Simulation Window")]
 	private static void openSimulationWindow() {
 		EditorWindow.GetWindow(typeof(SimulationWindow)).Show();
